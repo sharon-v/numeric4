9211a5fc298f0608bc3fa68671adb997ee95d52a
sharon <sharonvazana@gmail.com>
2021-05-14 16:37:30 +0300

    bisection method using range 0.01

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """ assignment 4
     team members: Hadar Amsalem, Sharon Vazana
     git link: https://github.com/sharon-v/numeric4.git"""
+import math
 
 import sympy as sp
 from sympy.utilities.lambdify import lambdify
@@ -12,7 +13,7 @@ from sympy.utilities.lambdify import lambdify
 # newton_raphson
 # secant_method
 
-
+# send polynom after lambdify
 def bisection_method(polynom, startPoint, endPoint, epsilon=0.0001):
     """
     :param polynom:
@@ -21,7 +22,44 @@ def bisection_method(polynom, startPoint, endPoint, epsilon=0.0001):
     :param epsilon:
     :return:
     """
-    pass
+    x = sp.symbols('x')
+    f = lambdify(x, polynom)
+    fTag = calcDerived(polynom)
+    error = calcError(startPoint, endPoint, epsilon)
+    i = startPoint
+    print("*** Bisection_Method ***")
+    while i < endPoint:
+        j = i + 0.01
+        c, iteration = runMethod(f, i, j, error, epsilon)
+        if c is not None:
+            print("root: " + str(c) + " ,\titeration: " + str(iteration))
+        c, iteration = runMethod(fTag, i, j, error, epsilon)    # derived
+        if c is not None:
+            if -epsilon < f(c) < epsilon:
+                print("root: " + str(c) + " ,\titeration: " + str(iteration))
+        i += 0.01
+    print("------------------------")
+
+
+def runMethod(f, startA, endB, error, epsilon=0.0001):
+    fXl = f(startA)
+    fXr = f(endB)
+    if (fXl * fXr) > 0:
+        return None, None
+    i = -1
+    c = startA
+    while (endB - startA > epsilon) and (i < error):
+        i += 1
+        # calc middleC
+        c = (startA + endB) / 2
+        if (f(startA) * f(c)) > 0:
+            startA = c
+        else:
+            endB = c
+    if i + 1 > error:
+        print("Could not find root, The function is not suitable for bisection method")
+        return None, None
+    return c, i
 
 
 def printDerived(my_f):
@@ -35,21 +73,48 @@ def printDerived(my_f):
     print("f'': ", d1)
 
 
-def calcDerived(f, x):
+def calcDerived(f):
     # calc the derivative from func -> lambdify
     # f = x ** 3 + 2 * x + 5
+    x = sp.symbols('x')
     f_prime = f.diff(x)
-    print("f : ", f)
-    print("f' : ", f_prime)
-    f = lambdify(x, f)
+    # print("f : ", f)
+    # print("f' : ", f_prime)
+    # f = lambdify(x, f)
     f_prime = lambdify(x, f_prime)
     # print("f(1):", f(1))
     # print("f'(1):", f_prime(1))
+    return f_prime
 
 
-def calcY(f, x):
-    return f(x)
+# def calcVal(f, val):
+#     x = sp.symbols('x')
+#     func = lambdify(x, f)
+#     return func(val)
 
 
-def calcError():
-    pass
+def calcError(start, end, epsilon=0.0001):
+    return -(math.log(epsilon / (end - start)) / math.log(2))
+
+
+def driver():
+    """
+    the main function of the program
+    """
+    x = sp.symbols('x')
+    # define polynom
+    # f = x ** 3 - x - 1
+    # f = x**2 - 1
+    f = x ** 4 + x ** 3 - 3 * x ** 2
+    # define range
+    # startPoint = -2
+    # endPoint = 2
+    startPoint = -3
+    endPoint = 2
+    # epsilon = pow(10, -10)
+    printDerived(f)
+    print("-------------")
+    bisection_method(f, startPoint, endPoint)
+
+
+driver()
